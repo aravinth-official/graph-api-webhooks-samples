12e65048b94af8a412bf1bce957b239774bf44d3
Aravinth Elangovan <57998320+aravinth-official@users.noreply.github.com>
2021-10-07 15:34:00 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -38,7 +38,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
-  console.log('Typeof request body:', );
+  console.log('Typeof request body:', typeof(req.body));
 
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
